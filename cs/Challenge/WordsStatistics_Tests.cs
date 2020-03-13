8a820ed32e4f0ca7d885fc7a898cd5cb6ed8692c
s4xack <sergmiron47@gmail.com>
2020-03-13 03:53:28 +0300

    comleted challange

--- cs/Challenge/WordsStatistics_Tests.cs
+++ cs/Challenge/WordsStatistics_Tests.cs
@@ -1,4 +1,6 @@
-﻿using FluentAssertions;
+﻿using System;
+using System.Linq;
+using FluentAssertions;
 using NUnit.Framework;
 
 namespace Challenge
@@ -41,7 +43,101 @@ namespace Challenge
 			wordsStatistics.GetStatistics().Should().HaveCount(2);
 		}
 
+		[Test]
+		public void AddWord_GotNull_ThrowArgumentNullException()
+		{
+			Action a = () => wordsStatistics.AddWord(null);
+			a.ShouldThrow<ArgumentNullException>();
+		}
 
-		// Документация по FluentAssertions с примерами : https://github.com/fluentassertions/fluentassertions/wiki
-	}
+		[Test]
+		public void AddWord_GotWhitespaces_CollectionShouldBeEmpty()
+		{
+			wordsStatistics.AddWord("   ");
+			wordsStatistics.GetStatistics().Count().Should().Be(0);
+		}
+
+		[Test]
+		public void AddWord_GotWordInUpperCase_ShouldContainWordInLowerCase()
+		{
+			wordsStatistics.AddWord("ABC");
+			wordsStatistics.GetStatistics().All(w => w.Word == w.Word.ToLower()).Should().Be(true);
+		}
+		
+		[Test]
+		public void GetStatistics_OrderIsCorrect()
+        { 
+			wordsStatistics.AddWord("d");
+			wordsStatistics.AddWord("b");
+			wordsStatistics.AddWord("c");
+			wordsStatistics.AddWord("c");
+			wordsStatistics.AddWord("c");
+			wordsStatistics.AddWord("b");
+			wordsStatistics.AddWord("a");
+			wordsStatistics.AddWord("a");
+			wordsStatistics.AddWord("a");
+            var r = wordsStatistics.GetStatistics();
+			r.SequenceEqual(r.OrderByDescending(wordCount => wordCount.Count)
+				.ThenBy(wordCount => wordCount.Word)).Should().BeTrue();
+		}
+
+		[Test]
+		public void AddWord_SpacesBeginningWithDifSymbolsAtTheEnd_ShouldBeWhole()
+		{
+			wordsStatistics.AddWord("          a");
+			wordsStatistics.AddWord("          b");
+			wordsStatistics.GetStatistics().Should().Equal(new WordCount("          ", 2));
+		}
+
+		[Test]
+		public void AddWord_SimilarBeginningBigAndTenSymbolsWord_ShouldBeWhole()
+		{
+			wordsStatistics.AddWord("         aa");
+			wordsStatistics.AddWord("         a");
+			wordsStatistics.GetStatistics().Should().Equal(new WordCount("         a", 2));
+		}
+
+		[Test, Timeout(250)]
+		public void AddWord_TimeoutTest()
+		{
+			for (int i = 0; i < 500000; ++i)
+			{
+				wordsStatistics.AddWord("abcdef");
+			}
+		}
+
+
+
+
+		
+        [Test]
+		public void AddWord_SimilarHashCodeWords_ShouldBeDifferent()
+		{
+			wordsStatistics.AddWord("xqzrbn");
+			wordsStatistics.AddWord("krumld");
+			wordsStatistics.GetStatistics().Count().Should().Be(2);
+        }
+		
+		[Test]
+		public void Test321()
+		{
+			wordsStatistics.AddWord("d");
+			wordsStatistics.AddWord("b");
+			wordsStatistics.AddWord("c");
+			wordsStatistics.AddWord("c");
+			wordsStatistics.AddWord("c");
+			wordsStatistics.AddWord("b");
+			wordsStatistics.AddWord("a");
+			wordsStatistics.AddWord("a");
+			wordsStatistics.AddWord("a");
+
+			var first = wordsStatistics.GetStatistics().ToArray();
+			var second = wordsStatistics.GetStatistics().ToArray();
+
+			first.SequenceEqual(second).Should().BeTrue();
+		}
+		
+
+        // Документация по FluentAssertions с примерами : https://github.com/fluentassertions/fluentassertions/wiki
+    }
 }
